7897b3292f49d464b9bc7dd02b938466cc754df0
wanko <kouji124@w4.dion.ne.jp>
2020-09-16 02:16:05 +0900

    added new file

---
+++ newfile.js
@@ -0,0 +1,9 @@
+let score = 10;
+
+if (score >= 70) {
+    console.log('You passed!');
+} else if ( score < 70 && score >= 50) {
+    console.log('You passed, but you need take a supplimentary class');
+} else {
+    console.log('You failed');
+}
