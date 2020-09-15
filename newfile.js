b7cedf3fdefebd2396aba087650a8389beb2da2e
wanko <kouji124@w4.dion.ne.jp>
2020-09-16 02:19:26 +0900

    added condition

--- newfile.js
+++ newfile.js
@@ -1,9 +1,11 @@
-let score = 10;
+let score = 90;
 
-if (score >= 70) {
+if (score <= 100 && score >= 70) {
     console.log('You passed!');
-} else if ( score < 70 && score >= 50) {
+} else if (score < 70 && score >= 50) {
     console.log('You passed, but you need take a supplimentary class');
-} else {
+} else if (score < 50 && score >=0) {
     console.log('You failed');
-}
+} else {
+    console.log('Score is not valid');
+};
